f0c7961a72455206b9f363c4cd4732dd8fabf1a2
marzieh802 <marzieh.ansari802@gmail.com>
2024-09-13 18:43:52 +0330

    added if

---
+++ control-flow.py
@@ -0,0 +1,8 @@
+nature = "mountains"
+
+if nature == "jungle":
+    print("Let's go to the jungle")
+elif(nature == "mountains"):
+    print("Let's go to the montains")
+else:
+    print("where should we go?")
