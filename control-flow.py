c6fcec57f5ae6adcaa9a7088f4a60c3adfdc5e0e
marzieh802 <marzieh.ansari802@gmail.com>
2024-09-13 18:54:43 +0330

    fixed typo

--- control-flow.py
+++ control-flow.py
@@ -3,6 +3,6 @@ nature = "mountains"
 if nature == "jungle":
     print("Let's go to the jungle")
 elif(nature == "mountains"):
-    print("Let's go to the montains")
+    print("Let's go to the mountains")
 else:
     print("where should we go?")
