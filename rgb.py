da63332419fc19643d5b02f6f86e152d6c8f3d0c
marzieh802 <marzieh.ansari802@gmail.com>
2024-10-14 11:17:41 +0330

    added rgb

---
+++ rgb.py
@@ -0,0 +1,11 @@
+def rgb():
+
+    print("\033[38;2;255;51;51m")  # text color
+    print("\033[48;2;128;223;255m")  # background color
+    # this codes name is ansi escape code
+
+
+if (__name__ == "__main__"):
+    rgb()
+    print("this is a test")
+    print("this is true color and")
