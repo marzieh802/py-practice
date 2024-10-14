8749c7946de665fcbf1f843816f5d3ec1878a5c4
Hosein Fathi <hfathi711@gmail.com>
2024-10-14 15:08:01 +0330

    added function for factorial

--- hosein.py
+++ hosein.py
@@ -1,4 +1,17 @@
-isEven = 10 % 2
+# isEven = 10 % 2
 
-if isEven:
-    print("10 is even")
+# if isEven:
+#     print("10 is even")
+
+def fact(num):
+
+    res = 1
+    i = 1
+    while (i <= num):
+        res *= i
+        i += 1
+    return res
+
+
+if (__name__ == "__main__"):
+    print(fact(10))
