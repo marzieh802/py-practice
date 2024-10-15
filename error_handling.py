b964a8ba4e94f82dcd6fa28a546ae3d92f2414ed
marzieh802 <marzieh.ansari802@gmail.com>
2024-10-15 18:51:53 +0330

    added try except homework

--- error_handling.py
+++ error_handling.py
@@ -6,8 +6,12 @@ def write_func():
                 "write function in mode 'w' is for writing and creating")
             print(content.read())
             print("end of try")
+    except TypeError:
+        print("in write func there is TypeError")
+    except IOError:
+        print("in write func there is IOError")
     except:
-        print("in write func there is an error")
+        print("in write func there is for all error")
     finally:
         print("this is finally")  # this is always works!
 
@@ -24,3 +28,38 @@ def get_num():
             break
         except:
             print("you entered a wrong input")
+
+# homework
+
+
+def problem1():
+    try:
+
+        for i in ['a', 'b', 'c']:
+            print(i**2)  # exception thrown by this line
+    except TypeError:
+        print("this is a TypeError from problem1 function")
+
+
+def problem2():
+    try:
+
+        x = 5
+        y = 0
+        z = x/y
+    except ZeroDivisionError:
+        print("this is a ZeroDivisionError from problem2 function")
+    finally:
+        print("All Done from problem2 function(finally)")
+
+
+def ask():
+    '''this function has an error that depends on input'''
+    while True:
+        # you have to use the functions' output
+        try:
+            num = int(input("Input an integer:"))
+            print("Thank you, your number squared is :", num**2)
+            break
+        except ValueError:
+            print("An error occurred! Please try again!")
--- start.py
+++ start.py
@@ -1,13 +1,15 @@
-from error_handling import write_func
+from error_handling import write_func, get_num, problem1, problem2, ask
 from error_handling import add as add_nums
-from error_handling import get_num
 
 if (__name__ == "__main__"):
-    try:
-        get_num()
-        write_func()  # it has try so in its layer the error get handled and there is no need to write inside another 'try'
-        print(add_nums(2, '4'))
-    except:
-        print("this except is for add_nums")  # error bubbling
+    # try:
+    # get_num()
+    # write_func()  # it has try so in its layer the error get handled and there is no need to write inside another 'try'
+    # print(add_nums(2, '4'))
+    # except:
+    #     print("this except is for add_nums")  # error bubbling
 
+    # problem1()
+    # problem2()
+    ask()
     print("program is exiting")
